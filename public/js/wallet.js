f42f8cb10acc3426638ad1fea8df09914e8c3cb0
Nik-Le <nikita.lechner@gmail.com>
2026-02-01 15:53:26 +0100

    Navigationsbar bei views/index angepasst

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -1,6 +1,6 @@
-// Funktion dient dazu, dass die Events erst gehoert werden wenn HTML DOM Fertig gebaut ist
 let form_visible = false;
 
+// Funktion dient dazu, dass die Events erst gehoert werden wenn HTML DOM Fertig gebaut ist
 document.addEventListener("DOMContentLoaded", function() {
     const addBtn = document.getElementById("add-button");
 
