d28b6597fc642c195a4781ac03f62cf38f303014
Nik <123022192+Nik-Le@users.noreply.github.com>
2026-02-02 18:52:53 +0100

    Kleinen Passwortgenerator erstellt

---
+++ public/js/password-manager.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const generatePasswordBtn = document.getElementById("password-generator");
+
+    generatePasswordBtn.addEventListener("click", generatePassword);
+})
+
+function generatePassword () {
+    let password = "";
+
+    for (let i = 0; i < 12; i++) {
+        password = password + String.fromCharCode(Math.floor(Math.random() * (126 - 33 + 1)) + 33);
+
+    }
+
+    console.log(password);
+    document.getElementById("password-input").value = password;
+}
