dd5005a0a53641567af7c1fd0deba1a1f07b7900
PS445 <philipp.schaude@gmx.de>
2026-02-04 00:02:27 +0100

    datenbank angepasst, Netzwerkfehler gefixt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,28 +6,30 @@ router.get('/register', (req, res) => {
 });
 router.post('/register', async(req, res) => {
     try {
-        const existingUser = await User.findOne({username: username});
-        if (!existingUser) {
-            return res.status(400).send("Dieser Benutzername ist schon vergeben{}");
+        const { username, email } = req.body;
+        const existingUser = await User.findOne({username: username, email: email});
+        if (existingUser) {
+            return res.status(400).json({error:"Dieser Benutzername ist schon vergeben"});
         }
         const newUser = new User({
             username: req.body.username,
-            password: req.body.password
+            password: req.body.password,
+            email: req.body.email
         });
     await newUser.save();
     console.log('User angelegt!');
-    res.redirect('/login');}
+    }
     catch(error) {
         console.error(error);
-        res.stats(500).send('Fehler bei Registrierung');
+        res.status(201).json({ message: "Erfolgreich registriert!" });
     }
     });
 router.post('/login', async (req, res) => {
     try {
-        const { username, password } = req.body;
+        const { username, password, email } = req.body;
 
         // sucht user in DB
-        const user = await User.findOne({ username: username });
+        const user = await User.findOne({ username: username, email: email });
 
         // existiert user
         if (user && user.password === password) {
