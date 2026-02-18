032e7c91f76e22d0e18f08642373ec179eaa45b3
Jakob-332 <J.Bohnert4@web.de>
2026-02-18 19:31:58 +0100

    w

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ const authController = require('../controllers/authController')
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 router.post('/logout', authController.logoutUser);
+//router.post('', aut)
 
 
 
